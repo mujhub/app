7a6b95a1dbe01bf581de0c4ba6640338451e5934
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2020-12-24 02:18:31 +0530

    minor style changes

--- src/components/Shared/InputBox.js
+++ src/components/Shared/InputBox.js
@@ -5,7 +5,11 @@ import {TextInput, useTheme} from 'react-native-paper';
 const InputBox = (props) => {
   const {colors} = useTheme();
   return (
-    <TextInput theme={{colors}} {...props}>
+    <TextInput
+      theme={{colors}}
+      {...props}
+      mode="outlined"
+      style={{marginVertical: 5}}>
       {props.children}
     </TextInput>
   );
--- src/components/Shared/PrimaryButton.js
+++ src/components/Shared/PrimaryButton.js
@@ -3,7 +3,11 @@ import React from 'react';
 import {Button} from 'react-native-paper';
 
 const PrimaryButton = (props) => {
-  return <Button {...props}>{props.children}</Button>;
+  return (
+    <Button {...props} mode="contained" style={{margin: 5}}>
+      {props.children}
+    </Button>
+  );
 };
 
 export default PrimaryButton;
