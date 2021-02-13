ee00325a08ca1ffd4235a7210ad37d30b3836e1b
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-02-14 01:08:23 +0530

    theming

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,4 +1,23 @@
-export const PRIMARY = '#e65100';
+export const PRIMARY = '#2F80ED';
+export const ACCENT = '#FC9A44';
+
+export const BACKGROUND = {
+  PRIMARY: '#080910',
+  SECONDARY: '#141621',
+};
+
+export const THEME = {
+  T1: '#FBFCFF',
+  T2: '#EAF2FD',
+  T3: '#ACCCF8',
+  T4: '#5999F1',
+  T5: '#448DEF',
+  T6: '#2A73D5',
+  T7: '#215AA6',
+  T8: '#174077',
+  T9: '#0E2647',
+  T10: '#050D18',
+};
 export const ORANGE = {
   T1: '#ffd180',
   T2: '#ffab40',
@@ -6,14 +25,14 @@ export const ORANGE = {
   T4: '#ff6d00',
 };
 export const GRAY = {
-  T1: '#eceff1',
-  T2: '#cfd8dc',
-  T3: '#b0bec5',
-  T4: '#90a4ae',
-  T5: '#78909c',
-  T6: '#607d8b',
-  T7: '#546e7a',
-  T8: '#455a64',
-  T9: '#37474f',
-  T10: '#263238',
+  T1: '#fff',
+  T2: '#f7fafc',
+  T3: '#edf2f7',
+  T4: '#e2e8f0',
+  T5: '#cbd5e0',
+  T6: '#718096',
+  T7: '#4a5568',
+  T8: '#2d3748',
+  T9: '#1a202c',
+  T10: '#000',
 };
