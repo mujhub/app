b28cebdfeb17f10bee50e1cb5ef78be4bb5671e8
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-02-16 00:50:24 +0530

    Added mode as prop for alt buttons

--- src/components/Shared/PrimaryButton.js
+++ src/components/Shared/PrimaryButton.js
@@ -2,16 +2,27 @@ import React from 'react';
 
 import {Button} from 'react-native-paper';
 
+import {ROUNDNESS} from '../../styles/theme';
+
 const PrimaryButton = (props) => {
   return (
     <Button
       {...props}
-      mode="contained"
-      style={{margin: 5}}
+      mode={props.mode}
+      style={{
+        marginVertical: 8,
+        borderRadius: ROUNDNESS / 4,
+        height: 40,
+      }}
       disabled={props.loading}>
       {props.loading ? props.loadingText : props.children}
     </Button>
   );
+};
+
+// in case we need non contained props
+PrimaryButton.defaultProps = {
+  mode: 'contained',
 };
 
 export default PrimaryButton;
