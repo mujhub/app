4961851606eb100d7b25a4c49ba3148b4de68caa
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-04-08 04:27:44 +0530

    back button to search box

--- src/components/Eateries/SearchBox.js
+++ src/components/Eateries/SearchBox.js
@@ -1,30 +1,65 @@
 import React from 'react';
 import {View, Text, Keyboard} from 'react-native';
-import {InputBox} from '../Shared';
+import {InputBox, Type} from '../Shared';
 import {OUTLETS} from '../../constants/strings';
+import {useTheme} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const SearchBox = ({
   searchQuery,
   setSearchQuery,
   isSearching,
   setIsSearching,
+  navigation,
 }) => {
+  const {colors} = useTheme();
+  const handleBack = () => {
+    setIsSearching(false);
+    setSearchQuery('');
+    Keyboard.dismiss();
+  };
+
   return (
-    <InputBox
-      value={searchQuery}
-      onChangeText={(value) => setSearchQuery(value)}
-      label={OUTLETS.SEARCH_BOX_PLACEHOLDER}
-      onFocus={() => {
-        setIsSearching(true);
-      }}
-      viewStyle={{marginBottom: 15, marginTop: 15, padding: 2}}
-      cancellable={isSearching}
-      onCancel={() => {
-        setIsSearching(false);
-        setSearchQuery('');
-        Keyboard.dismiss();
-      }}
-    />
+    <>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {searchQuery !== '' && isSearching ? (
+          <Icon
+            name="chevron-back"
+            color={colors.text}
+            size={24}
+            onPress={handleBack}
+            style={{
+              textAlignVertical: 'center',
+              marginRight: 10,
+              marginTop: 15,
+              marginLeft: 0,
+            }}
+          />
+        ) : null}
+
+        <InputBox
+          value={searchQuery}
+          onChangeText={(value) => setSearchQuery(value)}
+          label={OUTLETS.SEARCH_BOX_PLACEHOLDER}
+          onFocus={() => {
+            setIsSearching(true);
+          }}
+          viewStyle={{marginBottom: 15, flex: 1}}
+          cancellable={isSearching}
+          onCancel={() => {
+            setIsSearching(false);
+            setSearchQuery('');
+            Keyboard.dismiss();
+          }}
+        />
+      </View>
+    </>
   );
 };
 
