78d21719acc759b8890bc729562273b857798896
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-02-21 05:23:35 +0530

    fix! + roll out: andoird 11 linking queries + v7

--- src/constants/info.js
+++ src/constants/info.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = 6; // ROLL OUT UPDATE CHANGES
+export const APP_VERSION = 7; // ROLL OUT UPDATE CHANGES
 export const BUNDLE_IDENTIFIER = 'com.mujhub';
 export const SUPPORT_EMAIL = 'mujhub@gmail.com';
